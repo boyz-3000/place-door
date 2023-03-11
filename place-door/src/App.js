1a94c18fb991c28b4ea61b28c466165ab0f03e91
ayushr6 <ayushranwa6@gmail.com>
2023-03-11 12:58:47 +0530

    updated signin

--- place-door/src/App.js
+++ place-door/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Updates from "./Pages/Updates";
+import SigninForm from "./Pages/Signin/SigninForm";
 import Jobs from "./Pages/Jobs/Jobs";
 import Resume from "./Pages/Resume";
 import Applied from "./Pages/Applied/Applied";
 import Profile from "./Pages/Profile/Profile";
-import Signin from "./Pages/Signin";
 import AppliedStudent from "./components/comp-applied-stud/CompAppliedStud";
 
 import "./App.css";
@@ -16,12 +16,12 @@ function App(){
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Updates />} />
+          <Route path="/" element={<SigninForm />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/applied" element={<Applied />} />
+          <Route path="/profile" element={<Updates />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<Signin />} />
           <Route path="/applied-student" element={<AppliedStudent />} />
         </Routes>
       </BrowserRouter>
