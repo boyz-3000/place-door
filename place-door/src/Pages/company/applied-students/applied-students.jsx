72d28bbf517675f6071f037100444cc0387378a9
ayushr6 <ayushranwa6@gmail.com>
2023-03-14 13:21:42 +0530

    added top bar in applied-students

--- place-door/src/Pages/company/applied-students/applied-students.jsx
+++ place-door/src/Pages/company/applied-students/applied-students.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AppliedStudentsData from "./AppliedStudentsData";
 import CompAppliedStudDetails from "../../../components/company/comp-applied-stud/CompAppliedStudDetails";
-
+import TopBar from "../../../components/top-bar/TopBar";
 
 function CompAppliedStud (){
     return (
         <div>
+            < TopBar/>
             {
                 AppliedStudentsData.map((x)=>(
                     <CompAppliedStudDetails name={x.name} email={x.email} phone={x.phone} position={x.position} cgpa={x.cgpa} resume={x.resume} roll={x.roll} techstack={x.techstack}/>
