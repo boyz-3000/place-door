6e20813d8cf0b311a7d4c1195f61691d8c17e674
ayushr6 <ayushranwa6@gmail.com>
2023-03-30 03:38:05 +0530

    refactoring changes done

--- front-end/src/Pages/student/Resume/resume.jsx
+++ front-end/src/Pages/student/Resume/resume.jsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "../../../components/top-bar/TopBar";
 import { useReactToPrint } from "react-to-print"
 import "./resume.css"
 import { useRef } from 'react';
 
+const Resume = () => {
 
-const Resume=()=>{
-
-    const componentRef=useRef();
+    const resume = useRef();
     const handlePrint = useReactToPrint({
-        content:()=> componentRef.current,
-        documentTitle:'emp-data',
-        onAfterPrint:()=> alert("Print Sucessfull.")
+        content: () => resume.current,
+        documentTitle: 'emp-data',
+        // onAfterPrint:()=> alert("Print Sucessfull.")
     })
-    return(
+
+    const [backgroundImage, setBackgroundImage] = useState('https://pixlok.com/wp-content/uploads/2022/02/Profile-Icon-SVG-09856789.png');
+
+    const handleUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setBackgroundImage(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+    return (
         <>
-        <TopBar/>
-        <div ref={componentRef} className="main_resume container">
-            <div className="title">
-                <h1>Toyota Fortuner</h1>
+            <TopBar />
+            <div ref={resume} className="main_resume container">
+                <div className="title">
+                    <h1>Resume</h1>
+                </div>
+                <div className="resume">
+                    <div className="left">
+                        <div className="profile-img" style={{ backgroundImage: `url(${backgroundImage})` }} onClick={() => document.getElementById('fileInput').click()}>
+                            <input
+                                id="fileInput"
+                                type="file"
+                                accept="image/*"
+                                style={{ display: 'none' }}
+                                onChange={handleUpload}
+                            />
+                        </div>
+
+                        <div className="block">
+                            <div className="heading">
+                                {/* <p>Contact Details</p>
+                                <div className="heading-hr"/> */}
+                                <p className="hr-lines">CONTACT DETAILS</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="right">
+                        RIGHT
+                    </div>
+                </div>
             </div>
-            <div className="image">
-                <img src="https://imgs.search.brave.com/fzZ_PDlGerTE_O8fX6OYJgmWuufqwCgOVy7MZaBgRBE/rs:fit:1200:795:1/g:ce/aHR0cHM6Ly9jZG5p/LmF1dG9jYXJpbmRp/YS5jb20vVXRpbHMv/SW1hZ2VSZXNpemVy/LmFzaHg_bj1odHRw/czolMkYlMkZjZG5p/LmF1dG9jYXJpbmRp/YS5jb20lMkZFeHRy/YUltYWdlcyUyRjIw/MjEwMjE4MDM1OTA3/X0ZvcnR1bmVyX29m/Zl9yb2FkX3RyYWNr/aW5nXy5qcGcmaD03/OTUmdz0xMjAwJmM9/MQ"></img>
-                <img src="https://imgs.search.brave.com/fzZ_PDlGerTE_O8fX6OYJgmWuufqwCgOVy7MZaBgRBE/rs:fit:1200:795:1/g:ce/aHR0cHM6Ly9jZG5p/LmF1dG9jYXJpbmRp/YS5jb20vVXRpbHMv/SW1hZ2VSZXNpemVy/LmFzaHg_bj1odHRw/czolMkYlMkZjZG5p/LmF1dG9jYXJpbmRp/YS5jb20lMkZFeHRy/YUltYWdlcyUyRjIw/MjEwMjE4MDM1OTA3/X0ZvcnR1bmVyX29m/Zl9yb2FkX3RyYWNr/aW5nXy5qcGcmaD03/OTUmdz0xMjAwJmM9/MQ"></img>
-                <img src="https://imgs.search.brave.com/fzZ_PDlGerTE_O8fX6OYJgmWuufqwCgOVy7MZaBgRBE/rs:fit:1200:795:1/g:ce/aHR0cHM6Ly9jZG5p/LmF1dG9jYXJpbmRp/YS5jb20vVXRpbHMv/SW1hZ2VSZXNpemVy/LmFzaHg_bj1odHRw/czolMkYlMkZjZG5p/LmF1dG9jYXJpbmRp/YS5jb20lMkZFeHRy/YUltYWdlcyUyRjIw/MjEwMjE4MDM1OTA3/X0ZvcnR1bmVyX29m/Zl9yb2FkX3RyYWNr/aW5nXy5qcGcmaD03/OTUmdz0xMjAwJmM9/MQ"></img>
-                <img src="https://imgs.search.brave.com/fzZ_PDlGerTE_O8fX6OYJgmWuufqwCgOVy7MZaBgRBE/rs:fit:1200:795:1/g:ce/aHR0cHM6Ly9jZG5p/LmF1dG9jYXJpbmRp/YS5jb20vVXRpbHMv/SW1hZ2VSZXNpemVy/LmFzaHg_bj1odHRw/czolMkYlMkZjZG5p/LmF1dG9jYXJpbmRp/YS5jb20lMkZFeHRy/YUltYWdlcyUyRjIw/MjEwMjE4MDM1OTA3/X0ZvcnR1bmVyX29m/Zl9yb2FkX3RyYWNr/aW5nXy5qcGcmaD03/OTUmdz0xMjAwJmM9/MQ"></img>
-                <img src="https://imgs.search.brave.com/fzZ_PDlGerTE_O8fX6OYJgmWuufqwCgOVy7MZaBgRBE/rs:fit:1200:795:1/g:ce/aHR0cHM6Ly9jZG5p/LmF1dG9jYXJpbmRp/YS5jb20vVXRpbHMv/SW1hZ2VSZXNpemVy/LmFzaHg_bj1odHRw/czolMkYlMkZjZG5p/LmF1dG9jYXJpbmRp/YS5jb20lMkZFeHRy/YUltYWdlcyUyRjIw/MjEwMjE4MDM1OTA3/X0ZvcnR1bmVyX29m/Zl9yb2FkX3RyYWNr/aW5nXy5qcGcmaD03/OTUmdz0xMjAwJmM9/MQ"></img>
-            
-            
-            
-            </div>
-        </div>
-        <button onClick={handlePrint}> Press it</button>
+            <button onClick={handlePrint}> Press it</button>
 
         </>
     );
