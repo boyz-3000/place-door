e72e31f210903d88bf4dfa1796f9c198a5430573
ayushr6 <ayushranwa6@gmail.com>
2023-04-15 15:50:34 +0530

    filter functionality added for student-details

--- front-end/src/components/admin/details-table/DetailsTable.jsx
+++ front-end/src/components/admin/details-table/DetailsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import DetailsData from "./DetailsData";
 import "./DetailsTable.css";
 import axios from "axios";
@@ -6,6 +6,34 @@ import axios from "axios";
 function DetailsTable() {
 
   const [students, setStudents] = useState([]);
+
+  const [filters, setFilters] = useState({
+    studentName: "",
+    rollNo: "",
+    emailID: "",
+    phoneNo: "",
+    department: "",
+    stream: "",
+    cgpa: "",
+  });
+
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
+  };
+
+  const filteredStudents = students.filter((student) => {
+    // Filter the students based on the current filter values
+    return (
+      student.studentName.toLowerCase().includes(filters.studentName.toLowerCase()) &&
+      student.rollNo.toLowerCase().includes(filters.rollNo.toLowerCase()) &&
+      student.emailID.toLowerCase().includes(filters.emailID.toLowerCase()) &&
+      student.phoneNo.toLowerCase().includes(filters.phoneNo.toLowerCase()) &&
+      student.department.toLowerCase().includes(filters.department.toLowerCase()) &&
+      student.stream.toLowerCase().includes(filters.stream.toLowerCase()) &&
+      student.cgpa.toLowerCase().includes(filters.cgpa.toLowerCase())
+    );
+  });
 
   useEffect(() => {
     async function fetchStudents() {
@@ -33,27 +61,86 @@ function DetailsTable() {
               <th scope="col">Roll no.</th>
               <th scope="col">Email</th>
               <th scope="col">Phone no.</th>
-              <th scope="col">Batch</th>
+              <th scope="col">Department</th>
               <th scope="col">Stream</th>
               <th scope="col">CGPA</th>
             </tr>
+            <tr className="filter-row">
+              <td></td>
+              <td>
+                <input
+                  type="text"
+                  name="studentName"
+                  value={filters.studentName}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="rollNo"
+                  value={filters.rollNo}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="emailID"
+                  value={filters.emailID}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="phoneNo"
+                  value={filters.phoneNo}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="department"
+                  value={filters.department}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="stream"
+                  value={filters.stream}
+                  onChange={handleFilterChange}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="cgpa"
+                  value={filters.cgpa}
+                  onChange={handleFilterChange}
+                />
+              </td>
+            </tr>
           </thead>
           <tbody>
-            {students?.map((student, i)=>(
+            {filteredStudents?.map((student, i) => (
               <tr key={i}>
-                <th scope="row">{i+1}</th>
-                <td>{student.name}</td>
+                <th scope="row">{i + 1}</th>
+                <td>{student.studentName}</td>
                 <td>{student.rollNo}</td>
-                <td>{student.email}</td>
-                <td>{student.phone}</td>
-                <td>{student.batch}</td>
+                <td>{student.emailID}</td>
+                <td>{student.phoneNo}</td>
+                <td>{student.department}</td>
                 <td>{student.stream}</td>
                 <td>{student.cgpa}</td>
               </tr>
             ))}
           </tbody>
         </table>
-      </div>
+      </div >
     </>
   );
 }
